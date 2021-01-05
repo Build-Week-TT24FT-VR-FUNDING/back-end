4850acbbb9a3be043040a37dd1f111c94b01181f
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-01-04 23:20:12 -0800

    pg deploy issue

--- knexfile.js
+++ knexfile.js
@@ -1,27 +1,22 @@
 const sharedConfig = {
   client: "sqlite3",
   useNullAsDefault: true,
-  migrations: { directory: "./data/migrations" },
+
   pool: {
     afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
   },
+  migrations: { directory: "./data/migrations" },
+  seeds: { directory: "./data/seeds" },
 };
 
 module.exports = {
   development: {
     ...sharedConfig,
     connection: { filename: "./data/VRFund.db3" },
-    seeds: { directory: "./data/seeds" },
   },
 
   production: {
-    client: "pg",
+    ...sharedConfig,
     connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: { directory: "./data/migrations" },
-    seeds: { directory: "./data/seeds" },
   },
 };
