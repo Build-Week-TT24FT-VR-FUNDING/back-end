2976de04189478579b269b3b3dde4bb040e94d9a
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-01-04 22:52:02 -0800

    heroku wip

--- knexfile.js
+++ knexfile.js
@@ -17,6 +17,13 @@ module.exports = {
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
     migrations: { directory: "./data/migrations" },
     seeds: { directory: "./data/seeds" },
   },
